676c6ff6066ad4b8dd5d01680299a5037b10b7dd
kaylinkhanal10 <kyalin.khanal@gmail.com>
2023-12-31 10:41:54 +0545

    added id

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express()
 const port = 5000
 
 const users =[
-    {name:'ram', address:'ktm'},
-    {name:'kaylin', address:'gorkha'},
-    {name:'bishwo', address:'gorkha'},
-    {name:'gopal', address:'ktm'}
+    {id:323,name:'ram', address:'ktm'},
+    {id:511,name:'kaylin', address:'gorkha'},
+    {id:56,name:'bishwo', address:'gorkha'},
+    {id:456,name:'gopal', address:'ktm'}
 ]
 
 app.get('/users', (req, res) => {
